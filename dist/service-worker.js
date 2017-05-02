abe882323233fd9dacf5ece1638b2033ace2b7e9
Ding Peng <294420321@qq.com>
2017-05-02 17:46:10 -0400

    grunt

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-05-02T16:12:55.424Z';
+var CACHE_NAME = 'cache-v2017-05-02T21:45:32.636Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
