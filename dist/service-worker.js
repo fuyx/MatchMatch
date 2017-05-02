37d8edbb36eee243037d2e2c97145091fed85451
Yixian Fu <easonfu1994@gmail.com>
2017-05-01 23:13:51 -0400

    grunt

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-05-02T03:08:32.053Z';
+var CACHE_NAME = 'cache-v2017-05-02T03:13:21.141Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
